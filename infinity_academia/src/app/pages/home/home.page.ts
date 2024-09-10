a9c5ca8b75a64ad27bc0499b4890a4fe8ab02f59
RafaelSchneider91 <rafaelrafa91@hotmail.com>
2024-09-09 22:11:46 -0300

    add logout

--- infinity_academia/src/app/pages/home/home.page.ts
+++ infinity_academia/src/app/pages/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  
 
-  constructor() { }
+  constructor(
+    private authserive: AuthService
+  ) { }
 
   ngOnInit() {
+    ''
+  }
+
+  async logout(){
+    try {
+      await this.authserive.logout();
+    }catch(error){
+      console.error(error);
+    }
   }
 
 }
